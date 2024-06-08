31153f57b50cb86a5961bb97abafcdc337bd6e0b
davidmtz01 <95955080+davidmtz01@users.noreply.github.com>
2024-06-08 17:26:06 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,3 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -10,9 +8,13 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  server: {
+    port: 5173
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
+
